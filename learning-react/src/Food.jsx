5517003386e78012a53af0d7223fbbda32557548
Edward <ew010415@gmail.com>
2023-12-20 21:10:27 +0100

    Added React

---
+++ learning-react/src/Food.jsx
@@ -0,0 +1,15 @@
+function Food() {
+
+    const food1 = "Apple";
+    const food2 = "Kiwi";
+
+    return(
+        <ul>
+            <li></li>
+            <li>{food1.toUpperCase()}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food
